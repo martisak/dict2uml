a5622ccbb2e50a876b8ee1a6808f0419f91e3bc9
Martin Isaksson <martin.isaksson@gmail.com>
2017-11-03 23:38:45 +0100

    testcases

--- dict2uml/__init__.py
+++ dict2uml/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ['dict2uml', 'dict2svg']
 from dict2uml import dict2plantuml
 from dict2svg import dict2svg
+
+__all__ = ['dict2uml', 'dict2svg']
--- dict2uml/test/test_dict2uml.py
+++ dict2uml/test/test_dict2uml.py
@@ -3,4 +3,4 @@ from dict2uml import dict2plantuml
 
 def test_answer():
     assert dict2plantuml({}) == \
-    	'@startuml\nclass "root" as root {\n\t.. Properties ..\n}\n\n@enduml'
+        '@startuml\nclass "root" as root {\n\t.. Properties ..\n}\n\n@enduml'
